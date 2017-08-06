982a5507d3e919617d22426ad67f3dcb0143c584
Olga Anderjaska <olgaanderjaska@yahoo.com>
2017-08-06 18:03:30 -0400

    First atampt

---
+++ bamazonSupervisor.js
@@ -0,0 +1,13 @@
+//import libraries
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var Table = require('cli-table');
+
+// mySQL config variable
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon_db"
+});
